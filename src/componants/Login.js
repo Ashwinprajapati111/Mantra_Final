1b7e2a84198c6d620a89566f97bd07090e4e8d54
Ashwinprajapati111 <68985505+Ashwinprajapati111@users.noreply.github.com>
2023-04-21 18:42:14 +0530

    vz

--- src/componants/Login.js
+++ src/componants/Login.js
@@ -3,7 +3,7 @@ import "./main.css";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
 const Login = () => {
   return (
@@ -12,6 +12,56 @@ const Login = () => {
         <div className="login_main">
           <Typography variant="h5" gutterBottom>
             Login
+          </Typography>
+        </div>
+        <div className="txtfld">
+          <TextField
+            id="standard-basic"
+            fullWidth
+            label="Name"
+            variant="standard"
+          />
+        </div>
+        <div className="txtfld">
+          <TextField
+            id="standard-basic"
+            fullWidth
+            label="Password"
+            variant="standard"
+          />
+        </div>
+
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            ":hover": {
+              backgroundColor: "#3b4c8f",
+              color: "white",
+            },
+            color: "white",
+            backgroundColor: "#29335c",
+          }}
+        >
+          Login
+        </Button>
+        <Box
+          sx={{
+            p: 1,
+
+            fontSize: "0.875rem",
+            fontWeight: "700",
+          }}
+        >
+          <Typography variant="body2" gutterBottom>
+            Already registered ? <a className="logsmbtn">login</a>
+          </Typography>
+        </Box>
+      </div>
+      <div className="mainbox">
+        <div className="login_main">
+          <Typography variant="h5" gutterBottom>
+            Register
           </Typography>
         </div>
         <div className="txtfld">
@@ -47,20 +97,31 @@ const Login = () => {
           />
         </div>
 
-        <Button variant="contained" fullWidth>
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            ":hover": {
+              backgroundColor: "#3b4c8f",
+              color: "white",
+            },
+            color: "white",
+            backgroundColor: "#29335c",
+          }}
+        >
           Register
-        </Button>``
+        </Button>
         <Box
-        sx={{
-          p: 1,
-         
-          fontSize: '0.875rem',
-          fontWeight: '700',
-        }}
-      >
-        <Typography variant="body2" gutterBottom>
-          Already registered ? <a>login</a>
-        </Typography>
+          sx={{
+            p: 1,
+
+            fontSize: "0.875rem",
+            fontWeight: "700",
+          }}
+        >
+          <Typography variant="body2" gutterBottom>
+            Already registered ? <a className="logsmbtn">login</a>
+          </Typography>
         </Box>
       </div>
     </div>
